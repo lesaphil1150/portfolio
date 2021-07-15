9c881f6fe41f6027dfb4c38399d2294efe1f1ee8
lesaphil1150 <lesaphil1150@gmail.com>
2021-07-15 19:28:45 -0400

    first attempt

---
+++ index.js
@@ -0,0 +1,7 @@
+function scroll() {
+  let firstLast = document.querySelector(".first-last");
+  firstLast.style.animation =
+    "typing 3s steps(40, end), blink-caret 0.75s step-end 1";
+}
+// let resumeInfo = document.querySelector(".Resume-info");
+// resumeInfo.addEventListener("scroll", scroll, true);
